eed7715cb282d30a3d46eedf3946a1428698156f
Luca <eto.lucafrancesco@gmail.com>
2019-02-20 19:00:33 +0000

    All test passing

--- bowling.py
+++ bowling.py
@@ -1,11 +1,38 @@
 class Bowling:
     def __init__(self):
-        self._frames = []
+        self._frames = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: []}
+        self._current_roll = 1
         self._points = 0
 
-    def score(self):
-        return self._points
+    # def _populate_frames(self):
+    #     for frame in range(1, 13):
+    #         self._frames[frame] = []
 
     def roll(self, pins):
-        self._points += pins
-        return pins
+        if len(self._frames[self._current_roll]) == 0:
+            if pins == 10:
+                self._next_frame(pins)
+            else:
+                self._frames[self._current_roll].append(pins)
+        else:
+            self._next_frame(pins)
+
+    def score(self):
+        for i in range(1, 11):
+            if sum(self._frames[i]) < 10:
+                self._points += sum(self._frames[i])
+            elif sum(self._frames[i]) == 10 and len(self._frames[i]) == 2:
+                self._points += (sum(self._frames[i]) + self._frames[i+1][0])
+            else:
+                if len(self._frames[i+1]) > 1:
+                    self._points += (sum(self._frames[i]) + sum(self._frames[i+1]))
+                else:
+                    self._points += (sum(self._frames[i]) + sum(self._frames[i+1]) + sum(self._frames[i+2]))
+
+        return(self._points)
+
+
+
+    def _next_frame(self, pins):
+        self._frames[self._current_roll].append(pins)
+        self._current_roll += 1
--- test_bowling.py
+++ test_bowling.py
@@ -6,20 +6,17 @@ class TestBowling(unittest.TestCase):
     def setUp(self):
         self.game = Bowling()
 
-    def test_score(self):
-        self.assertEqual(self.game.score(), 0)
-
     # Gutter game scores zero
     # When you roll all misses, you get a total score of zero.
     def test_gutter_game(self):
-        for i in range(20):
+        for i in range(23):
             self.game.roll(0)
         self.assertEqual(self.game.score(), 0)
 
     # All ones scores 20
     # When you knock down one pin with each ball, your total score is 20
     def test_1_pin_each_roll(self):
-        for i in range(20):
+        for i in range(23):
             self.game.roll(1)
         self.assertEqual(self.game.score(), 20)
 
@@ -29,15 +26,35 @@ class TestBowling(unittest.TestCase):
         self.game.roll(4)
         self.game.roll(6)
         self.game.roll(3)
-        for i in range(17):
+        for i in range(20):
             self.game.roll(0)
         self.assertEqual(self.game.score(), 16)
 
     # A strike in the first frame, followed by three and then four pins,
     # followed by all misses, scores 24
+    def test_score_a_strike(self):
+        self.game.roll(10)
+        self.game.roll(3)
+        self.game.roll(4)
+        for i in range(20):
+            self.game.roll(0)
+        self.assertEqual(self.game.score(), 24)
 
+    # 3 strikes followed by all misses, scores 60
+    def test_score_3_strike(self):
+        self.game.roll(10)
+        self.game.roll(10)
+        self.game.roll(10)
+        for i in range(18):
+            self.game.roll(0)
+        self.assertEqual(self.game.score(), 60)
 
     # A perfect game (12 strikes) scores 300
+    def test_score_a_perfect_game(self):
+        for i in range(12):
+            self.game.roll(10)
+        print(self.game._frames)
+        self.assertEqual(self.game.score(), 300)
 
 
 if __name__ == '__main__':
